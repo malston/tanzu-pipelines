650bc4f04207c8661cc9e20b52ec185ed614b413
Mark Alston <malston@vmware.com>
2021-09-27 16:26:03 -0600

    Use kustomize edit to set the image

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -17,7 +17,8 @@ APP_NAME=${1:-spring-petclinic}
 ENV=${2:-dev}
 
 LATEST_IMAGE=$(kubectl get image spring-petclinic-image -n spring-petclinic -o jsonpath="{.status.latestImage}")
-CURRENT_IMAGE=$(yq e .spec.template.spec.containers[0].image "$SCRIPT_DIR/../argocd/$APP_NAME/$ENV/deployment.yaml")
-sed -i "s|$CURRENT_IMAGE|$LATEST_IMAGE|" "$SCRIPT_DIR/../argocd/$APP_NAME/$ENV/deployment.yaml"
+cd "$SCRIPT_DIR/../argocd/$APP_NAME/$ENV/"
+kustomize edit set image "$LATEST_IMAGE"
+cd -
 
 git diff "$SCRIPT_DIR/../argocd/$APP_NAME/$ENV/deployment.yaml"
