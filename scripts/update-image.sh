151af08ce3fc57210b3471bf1f7eb02dd0f0643e
Mark Alston <malston@vmware.com>
2021-09-27 15:33:47 -0600

    Add example to usage

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -6,14 +6,15 @@ set -o pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 usage(){
-	echo "Usage: $0 filename"
+	echo "Usage: $0 APP_NAME ENV"
+    echo "Example: $0 spring-petclinic dev"
 	exit 1
 }
 
 [[ $# -eq 0 ]] && usage
 
-APP_NAME=$1
-ENV=$2
+APP_NAME=${1:-spring-petclinic}
+ENV=${2:-dev}
 
 LATEST_IMAGE=$(kubectl get image spring-petclinic-image -n spring-petclinic -o jsonpath="{.status.latestImage}")
 CURRENT_IMAGE=$(yq e .spec.template.spec.containers[0].image "$SCRIPT_DIR/../argocd/$APP_NAME/$ENV/deployment.yaml")
